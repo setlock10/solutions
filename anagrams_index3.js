6d68d47198aaae68f8337b27f2fad2b9cc312624
John J Setlock II <johnsetlock@gmail.com>
2022-11-21 09:53:12 -0600

    Day 8

---
+++ anagrams_index3.js
@@ -0,0 +1,42 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+//
+// function anagrams(stringA, stringB)
+
+//     cleanA<-stringA   clean the strings - remove spaces and puncuation, make lowercase
+//     cleanB<-stringB
+
+//     tallyA<-{}  initialize as empty objects
+//     tallyB<-{}
+
+//     Calculate the tally for each string
+
+//     compare each key value pair in both objects
+//        return true if all are true
+//        return false if any is false
+
+   
+
+
+function anagrams(stringA, stringB) {
+
+    const cleanA=stringA.replace(/[^\w]/g,'').toLowerCase().split('').sort();
+    const cleanB=stringB.replace(/[\W]/g,'').toLowerCase().split('').sort();
+    
+    if (cleanA.length!==cleanB.length)
+        return false;
+
+    for (let i=0;i<cleanA.length;i++)
+        if (cleanA[i]!==cleanB[i])
+            return false;
+    return true;
+
+}
+module.exports = anagrams;
