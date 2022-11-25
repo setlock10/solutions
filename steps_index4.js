966ae2e0f5a5b842d3bbfd24d8bd73c9e3d0ef4e
John J Setlock II <johnsetlock@gmail.com>
2022-11-25 07:31:44 -0600

    Day 12

--- steps_index4.js
+++ steps_index4.js
@@ -26,12 +26,8 @@ function steps(n, row=0, stair='') {
         return steps(n, row+1);
     }
 
-    if (stair.length<=row){
-        stair+='#'
-    }else{
-        stair+=' ';
-    }
-    steps(n,row,stair)
+   const add = (stair.length<=row)?('#'):(' ');
+    steps(n,row,stair+add)
         
         
 
