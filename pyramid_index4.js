1e6373c50c7bdd31108b6585a8e61811e63a1133
John J Setlock II <johnsetlock@gmail.com>
2022-11-25 12:05:16 -0600

    Day 12

---
+++ pyramid_index4.js
@@ -0,0 +1,32 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left *and* right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+
+function pyramid(n,row=0,level='') {
+
+    if (n===row)
+        return;
+
+    if(n*2-1===level.length){
+        console.log(level)
+        return pyramid(n, row+1);
+    }
+
+    const add = ((level.length>=n-1-row)&&(level.length<=n-1+row))?('#'):(' ');
+    pyramid(n,row,level+add)
+    
+}
+
+module.exports = pyramid;
