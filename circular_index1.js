7a9fee3d3bfac27c3e4a13127d71c9563be4fb50
John J Setlock II <johnsetlock@gmail.com>
2022-11-30 07:49:21 -0600

    Day 17

---
+++ circular_index1.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Given a linked list, return true if the list
+// is circular, false if it is not.
+// --- Examples
+//   const l = new List();
+//   const a = new Node('a');
+//   const b = new Node('b');
+//   const c = new Node('c');
+//   l.head = a;
+//   a.next = b;
+//   b.next = c;
+//   c.next = b;
+//   circular(l) // true
+
+function circular(list) {
+    let slowNode = list.getFirst();
+    let fastNode = list.getFirst();
+
+    while (fastNode.next && fastNode.next.next){
+        fastNode = fastNode.next.next;
+        slowNode = slowNode.next;
+        if(slowNode===fastNode) return true;
+   }
+    return false;
+
+}
+
+module.exports = circular;
